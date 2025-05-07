2b8fea1442ba8c4244d76158baaaad2080ac048f
martin-o-r <martin.ore.rodriguez@gmail.com>
2025-05-06 22:59:53 -0400

    Update InfoScreen.java
    I tried to add a counter for the number of times the space key is pressed.

--- BdeB-H2025-Prog2/src/InfoScreen.java
+++ BdeB-H2025-Prog2/src/InfoScreen.java
@@ -4,6 +4,7 @@ import eko.EKOTouche;
 public abstract class InfoScreen extends ObjetJeu {
 
     private static boolean spaceIsPressed = false;
+    private int timesPressed = 0;
 
     /**
      * Construteur d'objet de type InfoScreen
@@ -22,23 +23,35 @@ public abstract class InfoScreen extends ObjetJeu {
      */
     @Override
     protected void mettreAJour(long deltaTemps) {
-
-        //boolean vraiment necessaire?
-
         /*
         chercher une methode avec un int, compteur qui determine combien de fois le SPACE a ete appuye
         pour continuer
          */
-
         if (EKOTouche.ESPACE.estEnfoncee()) {
-            EKO.attendre(300);
-            if (!spaceIsPressed) {
-                spaceIsPressed = true;
+            timesPressed++;
+            while (timesPressed < 2) {
                 GameProgressManager.next(this.etiquette);
-            } else {
-                spaceIsPressed = false; //reinitialise si la touche ESPACE est relachee
             }
         }
+        if (timesPressed >= 2) {
+            timesPressed = 0;
+        }
+
+
+
+
+        //boolean vraiment necessaire?
+        //methode avec un simple delai - mais le prof aime pas ca
+//        if (EKOTouche.ESPACE.estEnfoncee()) {
+//            //EKO.attendre(300);
+//            if (!spaceIsPressed) {
+//                spaceIsPressed = true;
+//
+//            } else {
+//                spaceIsPressed = false; //reinitialise si la touche ESPACE est relachee
+//            }
+//        }
+
     }
 
     /**
