3aa359897a90d7ef95677c0826fc8795c67746cc
martin-o-r <martin.ore.rodriguez@gmail.com>
2025-05-07 14:10:06 -0400

    Espace key needs a boolean
    Asked the teacher. Told me to detect when the space bar is RELEASED through a boolean!!

--- BdeB-H2025-Prog2/src/InfoScreen.java
+++ BdeB-H2025-Prog2/src/InfoScreen.java
@@ -5,7 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public abstract class InfoScreen extends ObjetJeu {
 
     private static boolean spaceIsPressed = false;
-    private int timesPressed = 0;
 
     /**
      * Construteur d'objet de type InfoScreen
@@ -24,36 +23,22 @@ public abstract class InfoScreen extends ObjetJeu {
      */
     @Override
     protected void mettreAJour(long deltaTemps) {
-        /*
-        chercher une methode avec un int, compteur qui determine combien de fois le SPACE a ete appuye
-        pour continuer
-         */
 
-        if (EKOTouche.ESPACE.estEnfoncee()) {
-            EKO.attendre(100);
-            timesPressed = 1;
-        } else {
-            timesPressed = 0;
-        }
-
-        if (timesPressed == 1) {
-            GameProgressManager.next(this.etiquette);
-        }
-
-
+        //a reessayer avec un boolean
+        //la touche doit etre detecte quand elle est relachee!!!!
 
         //tenter de controler la barre espace avec un boolean
         //boolean vraiment necessaire?
         //methode avec un simple delai - mais le prof aime pas ca
-//        if (EKOTouche.ESPACE.estEnfoncee()) {
-//            if (!spaceIsPressed) {
-//                spaceIsPressed = true;
-//                GameProgressManager.next(this.etiquette);
-//            } else {
-//                spaceIsPressed = false; //reinitialise si la touche ESPACE est relachee
-//            }
-//            EKO.attendre(300);
-//        }
+        if (EKOTouche.ESPACE.estEnfoncee()) {
+            if (!spaceIsPressed) {
+                spaceIsPressed = true;
+                GameProgressManager.next(this.etiquette);
+            } else {
+                spaceIsPressed = false; //reinitialise si la touche ESPACE est relachee
+            }
+            EKO.attendre(300);
+        }
     }
 
     /**
