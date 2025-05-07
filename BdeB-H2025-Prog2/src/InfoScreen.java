d69eda2d47861e70c0ba59c853c33c728017a1ab
martin-o-r <martin.ore.rodriguez@gmail.com>
2025-05-07 07:29:41 -0400

    Update InfoScreen.java
    I tried managing the first 3 intro screens with a counter, depending on how many times the space key was pressed. Didn't work.

--- BdeB-H2025-Prog2/src/InfoScreen.java
+++ BdeB-H2025-Prog2/src/InfoScreen.java
@@ -4,7 +4,7 @@ import eko.EKOTouche;
 public abstract class InfoScreen extends ObjetJeu {
 
     private static boolean spaceIsPressed = false;
-    private int timesPressed = 0;
+    //private int timesPressed = 0;
 
     /**
      * Construteur d'objet de type InfoScreen
@@ -27,31 +27,32 @@ public abstract class InfoScreen extends ObjetJeu {
         chercher une methode avec un int, compteur qui determine combien de fois le SPACE a ete appuye
         pour continuer
          */
+//        if (EKOTouche.ESPACE.estEnfoncee()) {
+//            timesPressed++;
+//        }
+//        if (timesPressed == 1) {
+//            GameProgressManager.next(this.etiquette);
+//            timesPressed = 0;
+//        }
+
+        //methode differente avec un delay
         if (EKOTouche.ESPACE.estEnfoncee()) {
-            timesPressed++;
-            while (timesPressed < 2) {
-                GameProgressManager.next(this.etiquette);
-            }
-        }
-        if (timesPressed >= 2) {
-            timesPressed = 0;
+            GameProgressManager.next(this.etiquette);
         }
 
 
-
-
+        //tenter de controler la barre espace avec un boolean
         //boolean vraiment necessaire?
         //methode avec un simple delai - mais le prof aime pas ca
 //        if (EKOTouche.ESPACE.estEnfoncee()) {
-//            //EKO.attendre(300);
 //            if (!spaceIsPressed) {
 //                spaceIsPressed = true;
-//
+//                GameProgressManager.next(this.etiquette);
 //            } else {
 //                spaceIsPressed = false; //reinitialise si la touche ESPACE est relachee
 //            }
+//            EKO.attendre(300);
 //        }
-
     }
 
     /**
