6b0a8f8e74f4c6d0f846efa3f3ec46d7246accc5
martin-o-r <martin.ore.rodriguez@gmail.com>
2025-05-09 19:20:06 -0400

    First implementation of tongue
    Learned I need a second timer to control the the flow of the tongue extending and retracting.

--- BdeB-H2025-Prog2/src/Frog.java
+++ BdeB-H2025-Prog2/src/Frog.java
@@ -3,15 +3,27 @@ import eko.EKOCouleur;
 
 public class Frog extends Enemy{
 
+    /*
+    Logique pour creer une animation en java
+    https://www.svetprogramiranja.com/animations_in_java_example.html
+        -> besoin d'un timer pour controler le deplacement de la langue?
+     */
+
     //Attributs
     private final String ICON = "\uEDF8";
-    private final String TONGUE1 = "\u2500";
-    private final String TONGUE2 = "\u257C";
+
+
+    private long waitBeforeMoving = 0;
+
+    private boolean extendedTongue = false;
+    private int tongueLength = 0;
+    private final int MAX_TONGUE_LENGTH = 3;
+    private long tongueTimer = 0;
 
     /**
-     * Constructeur
-     * @param x
-     * @param y
+     * Constructeur de l'objet de type Frog
+     * @param x Position X initiale du frog
+     * @param y Position Y initiale du frog
      */
     public Frog(int x, int y) {
         super("Frog", x, y, Etiquette.ENEMY);
@@ -19,11 +31,43 @@ public class Frog extends Enemy{
 
     @Override
     protected void mettreAJour(long deltaTemps) {
+        //Buffer qui permet de ralentir le mettreAJour
+        waitBeforeMoving += deltaTemps;
+        tongueTimer += deltaTemps;
+
+        if(waitBeforeMoving < 50) {
+            return;
+        }
+        waitBeforeMoving = 0;
+
+        if (tongueTimer < 500) {
+            extendedTongue = true;
+            if (tongueLength < MAX_TONGUE_LENGTH) {
+                tongueLength++;
+            }
+        } else if (tongueTimer < 1000) {
+            if (tongueTimer > 0) {
+                tongueLength--;
+            }
+            if (tongueLength == 0) {
+                extendedTongue = false;
+            }
+        } else {
+            tongueTimer = 0;
+        }
 
     }
 
     @Override
     protected void dessiner() {
-        EKOConsole.afficher(position.x, position.y, ICON, EKOCouleur.RVB(34, 139, 34));
+        EKOCouleur color = EKOCouleur.RVB(34, 139, 34);
+
+        EKOConsole.afficher(position.x, position.y, ICON, color); //dessin de la grenouille
+
+        if (extendedTongue) {
+            for (int i = 1; i <= tongueLength; i++) {
+                EKOConsole.afficher(position.x + i, position.y, "-", color);
+            }
+        }
     }
 }
---
+++ BdeB-H2025-Prog2/src/FrogTongue.java
@@ -0,0 +1,19 @@
+public class FrogTongue extends Enemy{
+
+    private final String TONGUE1 = "\u2500"; //corps de la langue
+    private final String TONGUE2 = "\u257C"; //pointe de la langue
+
+    public FrogTongue(int x, int y) {
+        super("Frog", x, y, Etiquette.ENEMY);
+    }
+
+    @Override
+    protected void mettreAJour(long deltaTemps) {
+
+    }
+
+    @Override
+    protected void dessiner() {
+
+    }
+}
