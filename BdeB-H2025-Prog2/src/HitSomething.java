d7005fc468a8372eca9f3d90ad02d3ddab27efb7
martin-o-r <martin.ore.rodriguez@gmail.com>
2025-05-10 16:16:10 -0400

    Update HitSomething.java
    Update comments and explanation

--- BdeB-H2025-Prog2/src/HitSomething.java
+++ BdeB-H2025-Prog2/src/HitSomething.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public abstract class HitSomething {
 
     /**
-     * Method qui permet de determiner si le prochain deplacement d'un objet est le meme que l'emplacement d'un mur
+     * Méthode qui permet de déterminer si le prochain déplacement d'un objet entre en collision avec un mur
      * @param x Position X d'un objet
      * @param y Position Y d'un objet
-     * @return Boolean qui determine si le prochain deplacement d'un objet est possible ou non
+     * @return Boolean qui determine si le prochain déplacement est valide
      */
     public static boolean didWeHitAWall(int x, int y) {
         ArrayList<ObjetJeu> walls = GestionnaireObjetsJeu.obtenir().trouverObjetsJeu(Etiquette.WALL);
@@ -20,6 +20,13 @@ public abstract class HitSomething {
         return false;
     }
 
+    /**
+     * Méthode qui permet de déterminer si le prochain déplacement d'un objet entre en collision avec une porte
+     * d'entrée ou une porte de sortie
+     * @param x Position X d'un objet
+     * @param y Position Y d'un objet
+     * @return Boolean qui determine si le prochain déplacement est valide
+     */
     public static boolean didWeHitADoor(int x, int y) {
         ObjetJeu entryDoor = GestionnaireObjetsJeu.obtenir().trouverObjetJeu("Entry door");
         ObjetJeu exitDoor = GestionnaireObjetsJeu.obtenir().trouverObjetJeu("Exit door");
